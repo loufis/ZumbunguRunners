cb105a82a6d3d820bba3cb0038a2e7e6086ccdab
Loufis <Loufis@Loufiss-MacBook-Air.local>
2016-04-04 18:42:37 -0700

    Initial Commit

---
+++ Assets/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class GameManager : NetworkManager {
+
+
+	public override void OnServerConnect(NetworkConnection conn)
+	{
+		Debug.Log (conn.address);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Host")) {
+			StartHost ();
+		}
+		else {
+			if (Input.GetButtonDown ("Client"))
+				StartClient ();
+			else if (Input.GetButtonDown ("Check"))
+				print (Network.isServer);
+		}
+	}
+
+}
