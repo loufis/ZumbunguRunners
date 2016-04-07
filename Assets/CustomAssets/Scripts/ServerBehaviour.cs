9c93ab0e1a533da13071724d6516e0df1a921f76
Loufis <Loufis@Loufiss-MacBook-Air.local>
2016-04-07 16:19:57 -0700

    Created Client and Server Behaviour Scripts

---
+++ Assets/CustomAssets/Scripts/ServerBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class ServerBehaviour : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	[Command]
+	public void Cmd_test(string x) {
+		Debug.Log (x);
+	}
+}
