e409768b2ddc8dbbbd22ff42ccd09eaafaae2da7
RoboticHuman <donomar@aucegypt.edu>
2016-04-14 11:25:20 +0200

    Board manager and sprites

---
+++ Assets/CustomAssets/Scripts/BoardManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoardManager : MonoBehaviour {
+
+    public Canvas canvas;
+    private RectTransform canvasSpace;
+
+
+    void Awake()
+    {
+        canvas = GetComponent<Canvas>();
+        canvasSpace = canvas.GetComponent<RectTransform>();
+    }
+
+    private void initializeLevel()
+    {
+        for(int i=0;i<canvasSpace.rect.width;i+=Globals.GROUND_TILESIZE)
+        {
+            
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/CustomAssets/Scripts/Globals.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Globals {
+
+    /*
+    #region Singleton
+    static Globals instance;
+    public static Globals Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new Globals();
+
+            return instance;
+        }
+    }
+    #endregion
+    */
+
+    public static int GROUND_TILESIZE = 128;
+}
