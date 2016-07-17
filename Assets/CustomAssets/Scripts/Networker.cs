6810e5e3f9bc2cf2fb8432efaedc1e7eb2d1e325
Loufis <Loufis@Loufiss-MacBook-Air.local>
2016-07-16 23:10:24 -0700

    Made the game work with unity multiplayer service

--- Assets/CustomAssets/Scripts/Networker.cs
+++ Assets/CustomAssets/Scripts/Networker.cs
@@ -7,6 +7,7 @@ public class Networker : NetworkManager {
     public List<GameObject> Player1Spawners;
     public List<GameObject> Player2Spawners;
     public GameObject Obstacle;
+	bool initiatedSpawnSequence;
     private void spawnObstacles()
     {
         NetworkServer.Spawn(Instantiate(Obstacle, Player1Spawners[1].transform.position, Quaternion.identity) as GameObject);
@@ -15,17 +16,18 @@ public class Networker : NetworkManager {
     }
 	// Use this for initialization
 	void Start () {
-		
+		initiatedSpawnSequence = false;
 	}
 
 	public override void OnServerConnect(NetworkConnection conn)
 	{
 		Debug.Log (conn.address);
 
-        if (NetworkServer.active)
+		if (NetworkServer.active && !initiatedSpawnSequence)
         {
+			initiatedSpawnSequence = true;
             Debug.Log("Starting The Spawn Sequence");
-            //spawnObstacles();
+            spawnObstacles();
         }
     }
 
